b8f378dcf11b1b58568f2b4390c48825ae2963ba
Truong Dung <Truong Dung>
2017-01-20 23:46:37 +0700

    f

---
+++ dynamic_listview_2001/__openerp__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Dynamic ListView Advance Odoo 9',
+    'summary': 'Change The Odoo List view On the fly without any technical knowledge',
+    'version': '1.0',
+    'category': 'Web',
+    'description': """
+
+    """,
+    'author': "Steven Cook",
+    'website': '',
+    'depends': ['web'],
+    'data': ['templates.xml',
+             'security/show_fields_security.xml',
+             'security/ir.model.access.csv'],
+    'price': 299.99,
+    'currency': 'EUR',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'qweb': ['static/src/xml/listview_button_view.xml'],
+    'images': [
+        'images/4.jpg',
+    ],
+}
